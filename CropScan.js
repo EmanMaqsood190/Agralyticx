635aad5ce65d19c39e29e3cd2ce7e330d91b18b3
Abeera Zainab <abeerazainab.work@gmail.com>
2026-09-07 20:07:43 +0500

    Delete CropScan.js

--- CropScan.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const cropScanSchema = new mongoose.Schema({
-  userId: { type: String, required: true }
-}, { strict: false });
-
-module.exports = mongoose.model('CropScan', cropScanSchema);
