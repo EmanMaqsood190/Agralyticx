58169632c275f45462c2257c35acae24df3b420b
Abeera Zainab <abeerazainab.work@gmail.com>
2026-09-07 20:11:34 +0500

    Delete LandRecord.js

--- LandRecord.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const landRecordSchema = new mongoose.Schema({
-  id: { type: String, required: true, unique: true },
-  userId: { type: String, required: true }
-}, { strict: false });
-
-module.exports = mongoose.model('LandRecord', landRecordSchema);
